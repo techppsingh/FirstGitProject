03741a5d4663d6866068e8b3a6c37089fbd155cf
prakash <prakashdims@gmail.com>
2024-02-09 11:11:47 +0530

    commit

---
+++ src/main/java/com/patna/pawan/SpringBootSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.patna.pawan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSimpleApplication.class, args);
+	}
+
+}
